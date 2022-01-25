94046af73e0bae1c86a37fedaa489c9960f39832
Dionisis Toulatos <47108480+dionisis2014@users.noreply.github.com>
2022-01-25 17:38:42 +0200

    fixed directory switch

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo pacman -S --needed --noconfirm base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ..
 yay -S --needed micro bash-completion
 cp -f ./bashrc ~/.bashrc
 yay -Rns go
