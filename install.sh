9c9682e8238d173a2cb3408b12437b23af340343
dionisis2014 <toulatosdionisis@gmail.com>
2022-01-25 15:26:19 +0200

    fixed cleanup

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo pacman -S --needed --noconfirm base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S micro bash-completion
+yay -S --needed micro bash-completion
 cp -f ./bashrc ~/.bashrc
-yay -Scc --noconfirm
+yay -Rns go
+yay -Scc
